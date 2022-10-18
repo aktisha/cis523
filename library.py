6c539e2e63e39b10c88b16be3a870d39256383d6
aktisha <aktilekzhumadilova@gmail.com>
2022-10-17 17:23:55 -0700

    Added transformers

--- library.py
+++ library.py
@@ -2,3 +2,127 @@ import pandas as pd
 import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
+
+class MappingTransformer(BaseEstimator, TransformerMixin):
+  
+  def __init__(self, mapping_column, mapping_dict:dict):
+    assert isinstance(mapping_dict, dict), f'{self.__class__.__name__} constructor expected dictionary but got {type(mapping_dict)} instead.'
+    self.mapping_dict = mapping_dict
+    self.mapping_column = mapping_column  #column to focus on
+
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+    assert self.mapping_column in X.columns.to_list(), f'{self.__class__.__name__}.transform unknown column "{self.mapping_column}"'  #column legit?
+    
+    #now check to see if all keys are contained in column
+    column_set = set(X[self.mapping_column])
+    keys_not_found = set(self.mapping_dict.keys()) - column_set
+    if keys_not_found:
+      print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] does not contain these keys as values {keys_not_found}\n")
+
+    #now check to see if some keys are absent
+    keys_absent = column_set -  set(self.mapping_dict.keys())
+    if keys_absent:
+      print(f"\nWarning: {self.__class__.__name__}[{self.mapping_column}] does not contain keys for these values {keys_absent}\n")
+
+    X_ = X.copy()
+    X_[self.mapping_column].replace(self.mapping_dict, inplace=True)
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+  class OHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=False):
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+  
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+    assert self.target_column in X.columns.to_list(), f'{self.__class__.__name__}.transform unknown column "{self.target_column}"'
+    
+    X_ = pd.get_dummies(X,
+                               prefix=self.target_column,
+                               prefix_sep='_',
+                               columns=[self.target_column],
+                               dummy_na=self.dummy_na,
+                               drop_first=self.drop_first
+                               )
+    
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+
+class OHETransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, target_column, dummy_na=False, drop_first=False):
+    self.target_column = target_column
+    self.dummy_na = dummy_na
+    self.drop_first = drop_first
+  
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+    assert self.target_column in X.columns.to_list(), f'{self.__class__.__name__}.transform unknown column "{self.target_column}"'
+    
+    X_ = pd.get_dummies(X,
+                               prefix=self.target_column,
+                               prefix_sep='_',
+                               columns=[self.target_column],
+                               dummy_na=self.dummy_na,
+                               drop_first=self.drop_first
+                               )
+    
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
+  
+class DropColumnsTransformer(BaseEstimator, TransformerMixin):
+  def __init__(self, column_list, action='drop'):
+    assert action in ['keep', 'drop'], f'{self.__class__.__name__} action {action} not in ["keep", "drop"]'
+    self.column_list = column_list
+    self.action = action
+
+  def fit(self, X, y = None):
+    print(f"\nWarning: {self.__class__.__name__}.fit does nothing.\n")
+    return X
+
+  def transform(self, X):
+    assert isinstance(X, pd.core.frame.DataFrame), f'{self.__class__.__name__}.transform expected Dataframe but got {type(X)} instead.'
+
+    columns_not_found = set(self.column_list) - set(X.columns.to_list())
+    X_ = X.copy()
+
+    
+    if self.action == 'keep':
+      if(set(self.column_list).issubset(set(X.columns.to_list()))) == False:
+        print("lala")
+        # change
+        assert set(self.column_list) <= set(X.columns.to_list()), f'{self.__class__.__name__}.the dataframe does not contain "{columns_not_found}" to keep.'
+      X_ = titanic_features[self.column_list]
+    elif self.action == 'drop':
+      if columns_not_found:
+        print(f"\nWarning: {self.__class__.__name__} does not contain {self.columns_not_found} to drop.\n")
+      X_ = X.drop(columns = self.column_list, errors = 'ignore')
+    
+    return X_
+
+  def fit_transform(self, X, y = None):
+    result = self.transform(X)
+    return result
