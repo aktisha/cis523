2773d4db5fa477895eb495afbd100e735a903253
aktisha <aktilekzhumadilova@gmail.com>
2022-11-06 23:29:53 -0800

    Added general, titanic and customer datasets setups

--- library.py
+++ library.py
@@ -268,3 +268,65 @@ def find_random_state(features_df, labels, n=200):
 
   idx = np.array(abs(var - rs_value)).argmin()  #find the index of the smallest value
   return idx
+
+#General dataset setup
+def dataset_setup(full_table, label_column_name:str, the_transformer, rs, ts=.2):
+  #your code below
+  from sklearn.model_selection import train_test_split
+
+  table_features = full_table.drop(columns=label_column_name)
+  labels = full_table[label_column_name].to_list()
+
+  X_train, X_test, y_train, y_test = train_test_split(table_features, labels, test_size=0.2, shuffle=True,
+                                                    random_state=rs, stratify=labels)
+  
+  X_train_transformed = the_transformer.fit_transform(X_train)
+  X_test_transformed = the_transformer.fit_transform(X_test)
+
+  x_trained_numpy = X_train_transformed.to_numpy()
+  x_test_numpy = X_test_transformed.to_numpy()
+  y_train_numpy = np.array(y_train)
+  y_test_numpy = np.array(y_test)
+
+  return x_trained_numpy, x_test_numpy, y_train_numpy, y_test_numpy
+
+#Titanic transformer
+titanic_transformer = Pipeline(steps=[
+    ('drop', DropColumnsTransformer(['Age', 'Gender', 'Class', 'Joined', 'Married',  'Fare'], 'keep')),
+    ('gender', MappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('class', MappingTransformer('Class', {'Crew': 0, 'C3': 1, 'C2': 2, 'C1': 3})),
+    ('ohe', OHETransformer(target_column='Joined')),
+    ('age', TukeyTransformer(column='Age', fence='outer')), #from chapter 4
+    ('fare', TukeyTransformer(column='Fare', fence='outer')), #from chapter 4
+    ('minmax', MinMaxTransformer()),  #from chapter 5
+    ('imputer', KNNTransformer())  #from chapter 6
+    ], verbose=True)
+
+#Titanic dataset setup
+def titanic_setup(titanic_table, transformer=titanic_transformer, rs=40, ts=.2):
+  x_trained_numpy, x_test_numpy, y_train_numpy,  y_test_numpy = dataset_setup(titanic_table, 'Survived',
+                                                                           titanic_transformer,
+                                                                           rs,
+                                                                           ts)
+  return x_trained_numpy, x_test_numpy, y_train_numpy,  y_test_numpy
+
+#Customer transformer
+customer_transformer = Pipeline(steps=[
+    ('id', DropColumnsTransformer(column_list=['ID'])),  #you may need to add an action if you have no default
+    ('os', OHETransformer(target_column='OS')),
+    ('isp', OHETransformer(target_column='ISP')),
+    ('level', MappingTransformer('Experience Level', {'low': 0, 'medium': 1, 'high':2})),
+    ('gender', MappingTransformer('Gender', {'Male': 0, 'Female': 1})),
+    ('time spent', TukeyTransformer('Time Spent', 'inner')),
+    ('minmax', MinMaxTransformer()),
+    ('imputer', KNNTransformer())
+    ], verbose=True)
+
+#Customer dataset setup
+def customer_setup(customer_table, transformer=customer_transformer, rs=76, ts=.2):
+  x_trained_numpy, x_test_numpy, y_train_numpy,  y_test_numpy = dataset_setup(customer_table, 'Rating',
+                                                                           customer_transformer,
+                                                                           rs,
+                                                                           ts)
+  return x_trained_numpy, x_test_numpy, y_train_numpy,  y_test_numpy
+
